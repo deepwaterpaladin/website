5a4e2354fab4a63f4cebd8172ce24fa3257a11d7
deepwaterpaladin <rielly.young11@gmail.com>
2024-05-04 17:17:35 -0400

    set function name

--- function/__init__.py
+++ function/__init__.py
@@ -5,4 +5,5 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     """
     Each request is redirected to the WSGI handler.
     """
+    func.__name__ = "Homepage"
     return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
